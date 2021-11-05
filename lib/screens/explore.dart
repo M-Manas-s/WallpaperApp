3d1b3eee50016dabf09128aa4bdfbea8d5d1026a
Vishal-Sankar <vishalsankar2002.gmail.com>
2021-11-05 19:14:58 +0530

    vs/added search bar and added networking part

--- lib/screens/explore.dart
+++ lib/screens/explore.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
 import 'package:wallpaperapp/constants/LocalUser.dart';
 import 'package:wallpaperapp/widgets/circulariconbutton.dart';
+import 'itempage.dart';
+import 'package:wallpaperapp/services/Networking.dart';
 
 class ExplorePage extends StatefulWidget {
   static String id = 'Explore_Page';
@@ -12,29 +14,47 @@ class ExplorePage extends StatefulWidget {
 }
 
 class _ExplorePageState extends State<ExplorePage> {
+  TextEditingController _controller = TextEditingController();
+  late String searchText;
+  var decodeddata;
   List<String> imageList = [
-    'images/img1.jpg',
-    'images/img2.jpg',
-    'images/img3.jpg',
-    'images/img4.jpg',
-    'images/img1.jpg',
-    'images/img2.jpg',
-    'images/img3.jpg',
-    'images/img4.jpg',
-    'images/img1.jpg',
-    'images/img2.jpg',
-    'images/img3.jpg',
-    'images/img4.jpg',
     'images/img1.jpg',
     'images/img2.jpg',
     'images/img3.jpg',
     'images/img4.jpg',
   ];
 
+  List<Image> gridimagelist = [];
+
   @override
   void initState() {
     super.initState();
     print(LocalUser.id);
+    getImages(
+        'https://api.unsplash.com/photos?per_page=12&client_id=Hl8nP0CKgfQztU1Y8Wb62YgydLAQSOQCnbnfZ2ueSHI');
+  }
+
+  Future<dynamic> getImages(String url) async {
+    NetworkHelper networkHelper = NetworkHelper(url);
+
+    var imagedata = await networkHelper.getWallpaper();
+    setState(() {
+      decodeddata = imagedata;
+    });
+    appenddatainList();
+  }
+
+  void appenddatainList() {
+    for (int i = 0; i < 9; i++) {
+      gridimagelist.add(Image.network(
+        decodeddata[i]['urls']['regular'],
+        fit: BoxFit.cover,
+      ));
+    }
+  }
+
+  void clearText() {
+    _controller.clear();
   }
 
   @override
@@ -48,38 +68,77 @@ class _ExplorePageState extends State<ExplorePage> {
             fontSize: 18.0, color: Colors.white, fontWeight: FontWeight.w300),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
         child: CustomScrollView(
           scrollDirection: Axis.vertical,
           slivers: [
+            SliverAppBar(
+              title: AppBar(
+                shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30.0))),
+                backgroundColor: Colors.black,
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: _controller,
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          fillColor: Colors.black,
+                          hintText: 'Search Eg. London, Food, etc',
+                          hintStyle: TextStyle(color: Colors.white60),
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: Colors.white,
+                          ),
+                          suffixIcon: IconButton(
+                              onPressed: () {
+                                clearText();
+                              },
+                              icon: Icon(Icons.clear,color: Colors.white,)),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              backgroundColor: Colors.black,
+            ),
             SliverList(
               delegate: SliverChildListDelegate(
                 [
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(30.0),
-                      child: ImageSlideshow(
-                        autoPlayInterval: 1,
-                        children: [
-                          Image.asset(
-                            'images/img1.jpg',
-                            fit: BoxFit.cover,
-                          ),
-                          Image.asset(
-                            'images/img2.jpg',
-                            fit: BoxFit.cover,
-                          ),
-                          Image.asset(
-                            'images/img3.jpg',
-                            fit: BoxFit.cover,
-                          ),
-                          Image.asset(
-                            'images/img4.jpg',
-                            fit: BoxFit.cover,
-                          ),
-                        ],
-                        isLoop: true,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white, width: 1.5),
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(30.0),
+                        child: ImageSlideshow(
+                          autoPlayInterval: 1,
+                          children: [
+                            Image.asset(
+                              'images/img1.jpg',
+                              fit: BoxFit.cover,
+                            ),
+                            Image.asset(
+                              'images/img2.jpg',
+                              fit: BoxFit.cover,
+                            ),
+                            Image.asset(
+                              'images/img3.jpg',
+                              fit: BoxFit.cover,
+                            ),
+                            Image.asset(
+                              'images/img4.jpg',
+                              fit: BoxFit.cover,
+                            ),
+                          ],
+                          isLoop: true,
+                        ),
                       ),
                     ),
                   ),
@@ -91,110 +150,121 @@ class _ExplorePageState extends State<ExplorePage> {
                         CircularIconButton(
                           onpressed: () {},
                           iconname: 'Top',
-                          icon: Icons.mood,
+                          icon: Icon(
+                            Icons.mood,
+                            color: Colors.white,
+                          ),
                           color: Colors.purple,
                         ),
                         CircularIconButton(
                           onpressed: () {},
                           iconname: 'Collections',
-                          icon: Icons.collections_bookmark_outlined,
-                          color: Colors.green,
+                          icon: Icon(
+                            Icons.collections_bookmark_outlined,
+                            color: Colors.white,
+                          ),
+                          color: Colors.red,
                         ),
                         CircularIconButton(
                           onpressed: () {},
                           iconname: 'Nature',
-                          icon: Icons.filter_vintage_outlined,
-                          color: Colors.red,
+                          icon: Icon(
+                            Icons.filter_vintage_outlined,
+                            color: Colors.white,
+                          ),
+                          color: Colors.green,
                         ),
                       ],
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: ListTile(
-                      title: Text(
-                        'Best out of all',
-                        style: TextStyle(fontSize: 18.0, color: Colors.white),
-                      ),
-                      subtitle: Text(
-                        'Special ones for you',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      trailing: Icon(
-                        Icons.chevron_right,
-                        color: Colors.white,
-                      ),
-                    ),
+                  TitleTile(
+                    title: 'Best out of all',
+                    subtitle: 'Special ones for you',
                   ),
-                  GridView.builder(
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    scrollDirection: Axis.vertical,
-                    padding: EdgeInsets.symmetric(horizontal: 18.0),
-                    itemCount: 12,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3,
-                        mainAxisSpacing: 18.0,
-                        crossAxisSpacing: 10.0,
-                        childAspectRatio: 1 / 2),
-                    itemBuilder: (_, index) {
-                      return GestureDetector(
-                        onTap: (){},
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10.0),
-                          child: Image.asset(
-                            imageList[index],
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: ListTile(
-                      title: Text(
-                        'Anime Wallpapers',
-                        style: TextStyle(fontSize: 18.0, color: Colors.white),
-                      ),
-                      subtitle: Text(
-                        'Ranges from Naruto toDeath Note',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      trailing: Icon(
-                        Icons.chevron_right,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  GridView.builder(
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    scrollDirection: Axis.vertical,
-                    padding: EdgeInsets.symmetric(horizontal: 18.0),
-                    itemCount: 12,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3,
-                        mainAxisSpacing: 18.0,
-                        crossAxisSpacing: 10.0,
-                        childAspectRatio: 1 / 2),
-                    itemBuilder: (_, index) {
-                      return GestureDetector(
-                        onTap: (){},
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10.0),
-                          child: Image.asset(
-                            imageList[index],
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      );
-                    },
-                  ),
+                  WallpaperGridBuilder(gridimagelist: gridimagelist),
                 ],
               ),
             )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class WallpaperGridBuilder extends StatelessWidget {
+  const WallpaperGridBuilder({
+    Key? key,
+    required this.gridimagelist,
+  }) : super(key: key);
+
+  final List<Image> gridimagelist;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      scrollDirection: Axis.vertical,
+      padding: EdgeInsets.symmetric(horizontal: 18.0),
+      itemCount: 9,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          mainAxisSpacing: 18.0,
+          crossAxisSpacing: 10.0,
+          childAspectRatio: 1 / 2),
+      itemBuilder: (_, index) {
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => ItemPage(Wallpaper: gridimagelist[index]),
+              ),
+            );
+          },
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10.0),
+            child:
+                gridimagelist.length == 0 ? Container() : gridimagelist[index],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class TitleTile extends StatelessWidget {
+  final String title;
+  final String subtitle;
+
+  const TitleTile({Key? key, required this.title, required this.subtitle})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+            border: Border.all(color: Colors.white, width: 1.0),
+            borderRadius: BorderRadius.circular(10.0)),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10.0),
+          child: ListTile(
+            title: Text(
+              title,
+              style: TextStyle(fontSize: 18.0, color: Colors.white),
+            ),
+            subtitle: Text(
+              subtitle,
+              style: TextStyle(color: Colors.white),
+            ),
+            trailing: Icon(
+              Icons.chevron_right,
+              color: Colors.white,
+            ),
+          ),
         ),
       ),
     );
