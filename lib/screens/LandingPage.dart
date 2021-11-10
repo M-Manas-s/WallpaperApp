4dae8a460b9ae14d0d577bfe4d875b0cb061c57c
Vishal-Sankar <vishalsankar2002.gmail.com>
2021-11-10 22:18:12 +0530

    vs//Debug Tag Removed

--- lib/screens/LandingPage.dart
+++ lib/screens/LandingPage.dart
@@ -5,23 +5,6 @@ import 'package:wallpaperapp/modals/WallpaperClass.dart';
 import 'package:wallpaperapp/screens/LikedScreen.dart';
 import 'ExplorePage.dart';
 
-// class LocalUserData extends InheritedWidget{
-//   final LocalUser localUser = LocalUser();
-//
-//   LocalUserData({
-//     Key? key,
-//     required Widget child,
-//   }) : super(key: key, child: child);
-//
-//   static LocalUserData of(BuildContext context) {
-//     final LocalUserData? result = context.dependOnInheritedWidgetOfExactType<LocalUserData>();
-//     assert(result != null, 'No FrogColor found in context');
-//     return result!;
-//   }
-//
-//   @override
-//   bool updateShouldNotify(LocalUserData old) => localUser != old.localUser;
-// }
 
 class LandingPage extends StatefulWidget {
   static String id = 'Default_Page';
